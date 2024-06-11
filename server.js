9c8c4a66ccbe93747c91c2e5a4e59dfc36eb0f53
rinkubangale <bangalehemant@gmail.com>
2024-06-11 22:53:13 +0530

    added home page

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ var express = require("express");
 var app = express();
 var http = require("http").createServer(app);
 
+app.use("/public", express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
+
 http.listen(3000, function () {
-    console.log("server started.")
+  console.log("server started.");
+  app.get("/", function (req, res) {
+    res.render("index");
+  });
 });
